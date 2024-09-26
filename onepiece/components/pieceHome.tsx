d48e01dc9f6c5197bd4694a86595dd93c935b202
Nomolos29 <nomolos2019@gmail.com>
2024-09-26 01:52:50 +0100

    components population

---
+++ onepiece/components/pieceHome.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface PieceHomeProps {
+  className: string;
+}
+
+const PieceHome = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
+
+export default PieceHome
